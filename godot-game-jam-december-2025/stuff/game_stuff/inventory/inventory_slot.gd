02d98e4819ef8c884ba860a6916fa6be0ed0bf21
Blurry <kuldeep.epk@gmail.com>
2025-12-26 18:03:24 +0530

    Updated Conversation UI Panel
    Updated Inventory UI Panel

--- godot-game-jam-december-2025/stuff/game_stuff/inventory/inventory_slot.gd
+++ godot-game-jam-december-2025/stuff/game_stuff/inventory/inventory_slot.gd
@@ -4,24 +4,16 @@ signal item_clicked(item: InventoryItem)
 
 var item: InventoryItem
 
-@onready var placeholder_poly = $MarginContainer/InventoryContainer/Polygon2D
 @onready var icon_rect = $MarginContainer/TextureRect
 
 
 func set_item(new_item: InventoryItem):
 	item = new_item
 	icon_rect.visible = false # Hide texture for now as we use poly
-	
-	if item.placeholder_color:
-		placeholder_poly.color = item.placeholder_color
-		placeholder_poly.visible = true
-	else:
-		placeholder_poly.visible = false
 
 	if item.icon:
 		icon_rect.texture = item.icon
 		icon_rect.visible = true
-		placeholder_poly.visible = false
 	
 	if has_node("NameLabel"):
 		$NameLabel.text = item.name
@@ -33,7 +25,6 @@ func clear():
 	item = null
 	icon_rect.texture = null
 	icon_rect.visible = false
-	placeholder_poly.visible = false
 	if has_node("NameLabel"):
 		$NameLabel.visible = false
 	disabled = true
